d499f22d414bc7c436cea8a59f46ba585bec5937
dmnddisanayaka <dmnddisanayaka@gmail.com>
2021-09-11 17:27:42 +0530

    Navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,7 +6,7 @@ class NavBar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Navbar</a>
+                        <a className="navbar-brand" href="#">MERN CRUD App</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                                 aria-label="Toggle navigation">
@@ -17,16 +17,7 @@ class NavBar extends Component {
                                 <li className="nav-item">
                                     <a className="nav-link active" aria-current="page" href="#">Home</a>
                                 </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Features</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Pricing</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link disabled" href="#" tabIndex="-1"
-                                       aria-disabled="true">Disabled</a>
-                                </li>
+
                             </ul>
                         </div>
                     </div>
