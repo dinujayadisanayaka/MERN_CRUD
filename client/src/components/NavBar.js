af5e227d35061e1922d4029ecf3723094f70191c
dmnddisanayaka <dmnddisanayaka@gmail.com>
2021-09-13 23:28:08 +0530

    naedit

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,12 +6,12 @@ class NavBar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
-                        {/*<a className="navbar-brand" href="#">MERN CRUD App</a>*/}
-                        {/*<button className="navbar-toggler" type="button" data-bs-toggle="collapse"*/}
-                        {/*        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"*/}
-                        {/*        aria-label="Toggle navigation">*/}
-                        {/*    <span className="navbar-toggler-icon"></span>*/}
-                        {/*</button>*/}
+                        <a className="navbar-brand" href="#">MERN CRUD App</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
                         <div className="collapse navbar-collapse" id="navbarNav">
                             <ul className="navbar-nav">
                                 <li className="nav-item">
